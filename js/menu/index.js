d8b3e72bbeaf362a225f073b80265261c2ad3d56
mouha221 <mouha.dev221@gmail.com>
2022-07-15 01:23:25 +0000

    hebergement 5 de RAKH

---
+++ js/menu/index.js
@@ -0,0 +1,63 @@
+
+
+const menu_bonbon = document.getElementById('menu-bonbon');
+const second_menu = document.getElementById('second-menu');
+
+	menu_bonbon.addEventListener('click', () => {
+		// console.log('yes');
+	second_menu.classList.toggle('show');
+});
+
+/****************************************************DEBUT BANIERE BOTTOM**********************************************/
+
+let img_slider = document.getElementsByClassName('img_slider');
+
+let etape = 0;
+
+let nbr_img = img_slider.length;
+
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
+
+
+function enleverActiveImages(){
+	for (let i = 0; i < nbr_img; i++) {
+		img_slider[i].classList.remove('active');
+	}
+}
+
+suivant.addEventListener('click', function(){
+	etape++;
+
+	if (etape >= nbr_img) {
+		etape = 0;
+	}
+
+	enleverActiveImages();
+	img_slider[etape].classList.add('active');
+})
+
+precedent.addEventListener('click', function(){
+	etape--;
+
+	if (etape < 0) {
+		etape = nbr_img - 1;
+	}
+
+	enleverActiveImages();
+	img_slider[etape].classList.add('active');
+})
+
+setInterval(function(){
+	etape++;
+
+	if (etape >= nbr_img) {
+		etape = 0;
+	}
+
+	enleverActiveImages();
+	img_slider[etape].classList.add('active');
+}, 3000)
+
+
+/****************************************************FIN BANIERE BOTTOM**********************************************/
---
+++ test/index.js
@@ -0,0 +1,10 @@
+
+// alert('ok');
+const img = document.getElementById('img');
+const menu_2 = document.getElementById('menu-2');
+
+img.addEventListener('click', () => {
+
+	// console.log('yes');
+	menu_2.classList.toggle('show');
+})
